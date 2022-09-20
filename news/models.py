e7bc3e53eee5fb4097478332215096d73ffcdfdd
Anthony <tonyudeagbala@gmail.com>
2022-09-20 22:22:48 +0100

    Add origin field

--- news/models.py
+++ news/models.py
@@ -1,13 +1,14 @@
+from django.utils import timezone
 from django.db import models
-from datetime import datetime
 
 class News(models.Model):
     url = models.URLField()
     author = models.CharField(max_length=200)
     title = models.CharField(max_length=200, default="none")
     type = models.CharField(max_length=200, default="none")
-    hacker_news_id = models.BigIntegerField(unique=True, primary_key=True)
-    date_created = models.DateTimeField(default=datetime.now())
+    hacker_news_id = models.BigIntegerField(unique=True)
+    origin = models.CharField(max_length=10, default="hackernews")
+    date_created = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return str(self.title)
@@ -18,8 +19,8 @@ class Comment(models.Model):
     news = models.ForeignKey(News, on_delete=models.CASCADE)
     text = models.TextField(default="Text")
     type = models.CharField(max_length=200, default="none")
-    hacker_news_id = models.BigIntegerField(unique=True, primary_key=True)
-    date_created = models.DateTimeField(default=datetime.now())
+    hacker_comment_id = models.BigIntegerField(unique=True)
+    date_created = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return str(self.text)
