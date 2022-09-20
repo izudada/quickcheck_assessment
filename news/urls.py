863c154f08185ab2d35b74a71a825dcda7a26d00
Anthony <tonyudeagbala@gmail.com>
2022-09-20 22:24:34 +0100

    feat: create article route

--- news/urls.py
+++ news/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import NewsApiListView
+from .views import article_view, NewsApiListView
 
 urlpatterns = [
-    # http://127.0.0.1:8080/api/v0/items/all --> lists all news id from hackernews endpoint
+    # http://127.0.0.1:8080/api/v0/items/all --> lists all news information from hackernews endpoint
     path('all/', NewsApiListView.as_view(), name='index'),
+
+    # http://127.0.0.1:8080/api/v0/items/all --> get an article using its unique hacker news id
+    path('item/<int:news_id>', article_view, name='article'),
 ]
