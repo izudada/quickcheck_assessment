326d0b261abb674096d4a465ee1442e099fd8bb3
Anthony <tonyudeagbala@gmail.com>
2022-09-20 14:28:07 +0100

    feature: Create NewsApiListView

--- news/views.py
+++ news/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.generics import ListAPIView
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.filters import SearchFilter, OrderingFilter
+from .models import Comment, News
+from .seralizers import NewsSerializer
 
-# Create your views here.
+
+class NewsApiListView(ListAPIView):
+    """
+        Api view that returns a paginated list of all articles
+    """
+    queryset = News.objects.all()
+    serializer_class = NewsSerializer
+    pagination_class = PageNumberPagination
+    filter_backends = (SearchFilter, OrderingFilter)
+    search_fields = ('type', 'title')
