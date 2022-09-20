8de9b5565c2bffca59cac9791f71fe8211956dee
Anthony <tonyudeagbala@gmail.com>
2022-09-20 14:26:23 +0100

    feature: Add REST_FRAMEWORK objects

--- config/settings.py
+++ config/settings.py
@@ -47,9 +47,11 @@ CRON_CLASSES = [
 ]
 
 REST_FRAMEWORK = {
-  'DEFAULT_PERMISSION_CLASSES': [
-      'rest_framework.permissions.AllowAny',
-  ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 20, 
 }
 
 MIDDLEWARE = [
