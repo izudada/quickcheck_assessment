4e4bbd636d12b6a72af626bfb892dbc25d4dbd30
Anthony <tonyudeagbala@gmail.com>
2022-09-22 15:46:46 +0100

    Edit import

--- news/urls.py
+++ news/urls.py
@@ -1,10 +1,21 @@
 from django.urls import path
-from .views import article_view, NewsApiListView
+from .views import (
+                        article_view, 
+                        comment_create_view,
+                        news_create_view, 
+                        NewsApiListView
+                    )
 
 urlpatterns = [
     # http://127.0.0.1:8080/api/v0/items/all --> lists all news information from hackernews endpoint
     path('all/', NewsApiListView.as_view(), name='index'),
 
-    # http://127.0.0.1:8080/api/v0/items/all --> get an article using its unique hacker news id
+    # http://127.0.0.1:8080/api/v0/items/item/id --> get an article using its unique hacker news id
     path('item/<int:news_id>', article_view, name='article'),
+
+    # http://127.0.0.1:8080/api/v0/items/item --> create an article
+    path('item/', news_create_view, name='new_article'),
+
+    path('item/<int:news_id>/comment/', comment_create_view, name='new_comment'),
+    
 ]
