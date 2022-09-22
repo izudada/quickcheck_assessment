d4be1fb9ca8e525d9e4aad1cd680ac55b438e103
Anthony <tonyudeagbala@gmail.com>
2022-09-22 16:41:03 +0100

    Create index route

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.urls import reverse, reverse_lazy
+from django.views.generic import (CreateView, DeleteView, DetailView, FormView,
+                                  ListView, TemplateView, UpdateView)
+from news.models import Comment, News
+import requests
 
-# Create your views here.
+
+class Index(ListView):
+    model = News
+    template_name = 'index.html'
+    fields = []
+
+    def get_queryset(self):
+        url = "http://127.0.0.1:8000/api/v0/items/all"
+        response = requests.get(url)
+        result = response.json()
+        print(result)
+        return result
