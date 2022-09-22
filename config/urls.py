1a1e157ebe269b0061f59e96342599b1d0923817
Anthony <tonyudeagbala@gmail.com>
2022-09-22 16:40:47 +0100

    Register blog urls

--- config/urls.py
+++ config/urls.py
@@ -19,5 +19,6 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path('api/v0/items/', include('news.urls'))
+    path('api/v0/items/', include('news.urls')),
+    path('', include('blog.urls'))
 ]
