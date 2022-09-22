5aed669bcfa68a508d0e626202169a0903319f73
Anthony <tonyudeagbala@gmail.com>
2022-09-22 15:42:57 +0100

    Create CoomentSerializer

--- news/models.py
+++ news/models.py
@@ -3,23 +3,24 @@ from django.db import models
 
 class News(models.Model):
     url = models.URLField()
-    author = models.CharField(max_length=200)
+    author = models.CharField(max_length=200, default="mainuser")
     title = models.CharField(max_length=200, default="none")
+    body = models.TextField(default="none")
     type = models.CharField(max_length=200, default="none")
-    hacker_news_id = models.BigIntegerField(unique=True)
+    hacker_news_id = models.BigIntegerField(unique=True, default=000)
     origin = models.CharField(max_length=10, default="hackernews")
     date_created = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
-        return str(self.title)
+        return str(self.id)
 
 
 class Comment(models.Model):
-    author = models.CharField(max_length=200)
-    news = models.ForeignKey(News, on_delete=models.CASCADE)
+    author = models.CharField(max_length=200, default="mainuser")
+    news = models.ForeignKey(News, on_delete=models.CASCADE, null=True)
     text = models.TextField(default="Text")
     type = models.CharField(max_length=200, default="none")
-    hacker_comment_id = models.BigIntegerField(unique=True)
+    hacker_comment_id = models.BigIntegerField(unique=True, default=000)
     date_created = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
--- news/seralizers.py
+++ news/seralizers.py
@@ -13,3 +13,46 @@ class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
         model = News
         fields = '__all__'
+
+
+class CreateNewsSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = News
+        fields = ['title', 'body', 'type', 'hacker_news_id', 'origin']
+    
+    def save(self, uuid):
+        """
+            A method overiding DRF serializer's save method
+        """
+        news = News(
+            title = self.validated_data['title'],
+            body = self.validated_data['body'],
+            type = self.validated_data['type'],
+            hacker_news_id = uuid,
+            origin = self.validated_data['origin']
+        )
+        news.save()
+
+        return news
+
+
+class CreateCommentSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Comment
+        fields = ['news', 'text', 'type', 'hacker_comment_id']
+    
+    def save(self, article, uuid):
+        """
+            A method overiding DRF serializer's save method
+        """
+        comment = Comment(
+            news = article,
+            text = self.validated_data['text'],
+            type = self.validated_data['type'],
+            hacker_comment_id = uuid
+        )
+        comment.save()
+
+        return comment
