f054d5cd4761a1d742505834a212ac7624bbf1f1
Anthony <tonyudeagbala@gmail.com>
2022-09-24 18:00:36 +0100

    Edit urls

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,13 @@
 from django.urls import include, path
 from .views import (
-                        Index
+                        Index,
+                        Article,
+                        get_comments
                     )
 
 
 urlpatterns = [
     path('', Index.as_view(), name='index'),
+    path('<int:pk>/', Article.as_view(), name='article_detail'),
+    path('comment/', get_comments, name='comments')
 ]
