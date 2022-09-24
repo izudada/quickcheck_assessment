95e08de391ebc28de67bb100ed5156bd171570a6
Anthony <tonyudeagbala@gmail.com>
2022-09-24 18:00:57 +0100

    Create get_comments views

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import redirect, render
 from django.urls import reverse, reverse_lazy
+from django.http import JsonResponse
 from django.views.generic import (CreateView, DeleteView, DetailView, FormView,
                                   ListView, TemplateView, UpdateView)
+                                  
 from news.models import Comment, News
 import requests
 
@@ -11,9 +13,40 @@ class Index(ListView):
     template_name = 'index.html'
     fields = []
 
-    def get_queryset(self):
-        url = "http://127.0.0.1:8000/api/v0/items/all"
-        response = requests.get(url)
-        result = response.json()
-        print(result)
-        return result
+    def get_context_data(self):
+        context = self.model.objects.all()
+        return {'context': context}
+
+
+class Article(DetailView):
+    model = News
+    template_name = 'article.html'
+
+    def get_context_data(self, **kwargs):
+        context = {}
+        article = News.objects.get(id=self.kwargs["pk"])
+        context['article'] = article
+
+        comments = article.get_comments()
+        context['comments'] = comments
+        
+        return context
+
+
+def get_comments(request):
+    if request.is_ajax():
+        news_id = request.GET.get('id');
+        article = News.objects.get(id=news_id)
+        comments = article.get_comments()
+        commment_holder = {}
+        comment_array = []
+        data = {}
+        for comment in comments:
+            commment_holder['author'] = comment.author
+            commment_holder['text'] = comment.text
+            commment_holder['type'] = comment.type
+            commment_holder['hacker_comment_id'] = comment.hacker_comment_id
+            commment_holder['date_created'] = comment.date_created
+            comment_array.append(commment_holder)
+        data['comments'] = comment_array
+        return JsonResponse(data)
